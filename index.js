f745c2978598cf37a2b74930e10930248a0d7d1a
UkazPromise <ukazpromise@gmail.com>
2022-01-11 14:30:49 +0100

    updates
    updates

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path = require('path');
 
-//initializing express.js
+// declare static path
+let staticPath = path.join(__dirname, "public");
 
+//initializing express.js
 const app = express();
+
+//middlewares
+app.use(express.static(staticPath));
 
 //routes
 //home route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(staticPath, "index.html"));
 })
 
 app.listen(3000, () => {
